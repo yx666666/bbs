1be56793301cad1a9e589c5fb591ecfdc8edcaaa
wuchong <wuchong@qq.com>
2019-04-29 20:35:19 +0800

    完成了帖子修改功能

--- post/views.py
+++ post/views.py
@@ -10,11 +10,25 @@ def create_post(request):
         title = request.POST.get('title')
         content = request.POST.get('content')
         post = Post.objects.create(title=title,content=content)
-        print post.id
+
         return redirect('/post/read/?post_id=%s' % post.id)
     return render(request,'create_post.html',{})
 def edit_post(request):
-    return render(request,'edit_post.html',{})
+    if request.method == 'POST':
+        post_id = int(request.POST.get('post_id'))
+        post = Post.objects.get(pk=post_id)
+        post.title = request.POST.get('title')
+        post.content = request.POST.get('content')
+        post.save()
+        return redirect('/post/read/?post_id=%s' % post.id)
+
+    else:
+        post_id = int(request.GET.get('post_id'))
+        post = Post.objects.get(pk=post_id)
+        return render(request, 'edit_post.html', {'post':post})
+
+
+
 def read_post(request):
     post_id = int(request.GET.get('post_id'))
     post = Post.objects.get(pk=post_id)
